117a8cd92f53f66b9d09ee4826789462936c5f45
Victor Hugo <vhlcads@gmail.com>
2025-10-25 15:21:16 -0300

    feat(backend): add initial ai test script with ollama

---
+++ backend/main.py
@@ -0,0 +1,19 @@
+from langchain_ollama import OllamaLLM
+from langchain_core.prompts import PromptTemplate
+
+llm = OllamaLLM(model="mistral")
+prompt  = PromptTemplate.from_template("Usuário: {pergunta}\nIA:")
+chain = prompt | llm 
+
+print("chatbot deepseek iniciado")
+print("Digite sua pergunta ou 'sair' para encerrar.")
+
+while True: 
+    pergunta = input("Você: ")
+    if pergunta.lower() in ['sair', 'exit', 'quit']:
+        print("Encerrando o chatbot. Até logo!")
+        break
+    resposta = chain.invoke({"pergunta": pergunta})
+    print(f"IA: {resposta}\n")
+    
+
