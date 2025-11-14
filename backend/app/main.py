f3a8946436a72261bfa285697f0a78d753bc672f
Victor Hugo <vhlcads@gmail.com>
2025-11-14 00:30:56 -0300

    feat(backend):enable CORS middleware for frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,7 +6,18 @@ app = FastAPI(
     description="Backend para o Chat-Call com IA",
     version="0.1.0"
 )
-
+origins = [
+    "http://localhost",
+    "http://localhost:3000", # Porta padrão do create-react-app
+    "http://localhost:5173", # Porta padrão do Vite
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 def read_root():
     return {"status": "Chat API está online!"}
